b4c95a23ca811e3276ba7a0fccd9618591cb9ab8
kshoker12 <karandeep.shoker@outlook.com>
2026-03-17 22:08:15 -0700

    Added gallery images

--- src/components/HeaderSubText.jsx
+++ src/components/HeaderSubText.jsx
@@ -1,3 +1,3 @@
 export default function HeaderSubText({ className = '', children }) {
-  return <p className={`text-center text-gray-600 ${className}`}>{children}</p>;
+  return <div className={`text-center text-gray-600 ${className}`}>{children}</div>;
 }
